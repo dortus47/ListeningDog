9638b88b97e46b49049f5b2dd6508202ba9510e5
dortus47 <dortus47@naver.com>
2023-03-22 01:20:00 +0900

    기본 프로젝트 파일 추가, StatusBar 초안 추가

---
+++ ListeningDog/ListeningDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListeningDogApp.swift
+//  ListeningDog
+//
+//  Created by 장은석 on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ListeningDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
